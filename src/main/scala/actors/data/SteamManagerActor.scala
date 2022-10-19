86f12d3f7d14c73efd015df1fc420e85aecdb6f0
JosueGalRe <josuegalre@gmail.com>
2022-10-19 14:48:49 -0600

    :recycle: Removed validation logic from HTTP layer, moved to intermediary layer

--- src/main/scala/actors/data/SteamManagerActor.scala
+++ src/main/scala/actors/data/SteamManagerActor.scala
@@ -2,14 +2,15 @@ package dev.galre.josue.akkaProject
 package actors.data
 
 import actors.game.GameActor.GameState
-import actors.game.GameManagerActor.CreateGameFromCSV
 import actors.review.ReviewActor.ReviewState
-import actors.review.ReviewManagerActor.CreateReviewFromCSV
 import actors.user.UserActor.UserState
-import actors.user.UserManagerActor.CreateUserFromCSV
 
 import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
+import akka.pattern.{ ask, pipe }
 import akka.util.Timeout
+import cats.data.EitherT
+
+import scala.concurrent.{ ExecutionContext, Future }
 
 object SteamManagerActor {
 
@@ -27,12 +28,61 @@ object SteamManagerActor {
     game:   GameState
   )
 
+  case class BasicUser(userId: Long, name: Option[String] = None)
+
+  object ComposedReview {
+    def apply(review: ReviewState, game: GameState, user: UserState): ComposedReview =
+      ComposedReview(
+        reviewId = review.reviewId,
+        steamApp = game,
+        author = BasicUser(user.userId, user.name),
+        region = review.region,
+        timestampCreated = review.timestampCreated,
+        timestampUpdated = review.timestampUpdated,
+        review = review.review,
+        recommended = review.recommended,
+        votesHelpful = review.votesHelpful,
+        votesFunny = review.votesFunny,
+        weightedVoteScore = review.weightedVoteScore,
+        commentCount = review.commentCount,
+        steamPurchase = review.steamPurchase,
+        receivedForFree = review.receivedForFree,
+        writtenDuringEarlyAccess = review.writtenDuringEarlyAccess,
+        authorPlaytimeForever = review.authorPlaytimeForever,
+        authorPlaytimeLastTwoWeeks = review.authorPlaytimeLastTwoWeeks,
+        authorPlaytimeAtReview = review.authorPlaytimeAtReview,
+        authorLastPlayed = review.authorLastPlayed
+      )
+  }
+
+  case class ComposedReview(
+    reviewId:                   Long,
+    steamApp:                   GameState,
+    author:                     BasicUser,
+    region:                     Option[String] = None,
+    review:                     Option[String] = None,
+    timestampCreated:           Option[Long] = None,
+    timestampUpdated:           Option[Long] = None,
+    recommended:                Option[Boolean] = None,
+    votesHelpful:               Option[Long] = None,
+    votesFunny:                 Option[Long] = None,
+    weightedVoteScore:          Option[Double] = None,
+    commentCount:               Option[Long] = None,
+    steamPurchase:              Option[Boolean] = None,
+    receivedForFree:            Option[Boolean] = None,
+    writtenDuringEarlyAccess:   Option[Boolean] = None,
+    authorPlaytimeForever:      Option[Double] = None,
+    authorPlaytimeLastTwoWeeks: Option[Double] = None,
+    authorPlaytimeAtReview:     Option[Double] = None,
+    authorLastPlayed:           Option[Double] = None,
+  )
+
   def props(
     gameManagerActor:   ActorRef,
     userManagerActor:   ActorRef,
     reviewManagerActor: ActorRef
   )
-    (implicit timeout: Timeout): Props =
+    (implicit timeout: Timeout, executionContext: ExecutionContext): Props =
     Props(
       new SteamManagerActor(
         gameManagerActor, userManagerActor, reviewManagerActor
@@ -42,13 +92,99 @@ object SteamManagerActor {
 
 class SteamManagerActor(
   gameManagerActor: ActorRef, userManagerActor: ActorRef, reviewManagerActor: ActorRef
-)(implicit timeout: Timeout)
+)(implicit timeout: Timeout, executionContext: ExecutionContext)
   extends Actor
   with ActorLogging {
 
+  import actors.game.GameActor._
+  import actors.game.GameManagerActor._
+  import actors.review.ReviewActor._
+  import actors.review.ReviewManagerActor._
+  import actors.user.UserActor._
+  import actors.user.UserManagerActor._
+
   import SteamManagerActor._
 
+  def getComposedReview(reviewId: Long): Future[Either[String, ComposedReview]] = {
+    val composedReviewFuture = for {
+      review <- EitherT((reviewManagerActor ? GetReviewInfo(reviewId)).mapTo[ReviewCreatedResponse])
+      game <- EitherT((gameManagerActor ? GetGameInfo(review.steamAppId)).mapTo[GetGameInfoResponse])
+      user <- EitherT((userManagerActor ? GetUserInfo(review.authorId)).mapTo[GetUserInfoResponse])
+    } yield {
+      ComposedReview(review, game, user)
+    }
+
+    composedReviewFuture.value
+  }
+
   override def receive: Receive = {
+    // All user messages
+    case createUserCommand: CreateUser =>
+      userManagerActor.forward(createUserCommand)
+
+    case getUserCommand: GetUserInfo =>
+      userManagerActor.forward(getUserCommand)
+
+    case updateUserCommand: UpdateUser =>
+      userManagerActor.forward(updateUserCommand)
+
+    case deleteUserCommand: DeleteUser =>
+      userManagerActor.forward(deleteUserCommand)
+
+    // All game messages
+    case createGameCommand: CreateGame =>
+      gameManagerActor.forward(createGameCommand)
+
+    case getGameCommand: GetGameInfo =>
+      gameManagerActor.forward(getGameCommand)
+
+    case updateGameCommand: UpdateName =>
+      gameManagerActor.forward(updateGameCommand)
+
+    case deleteGameCommand: DeleteGame =>
+      gameManagerActor.forward(deleteGameCommand)
+
+    // All review messages
+    case createCommand @ CreateReview(review) =>
+      val steamAppId = review.steamAppId
+      val authorId   = review.authorId
+
+      val createReviewFuture = for {
+        game <- EitherT((gameManagerActor ? GetGameInfo(steamAppId)).mapTo[GetGameInfoResponse])
+        user <- EitherT((userManagerActor ? GetUserInfo(authorId)).mapTo[GetUserInfoResponse])
+        review <- EitherT((reviewManagerActor ? createCommand).mapTo[ReviewCreatedResponse])
+        _ <- EitherT((userManagerActor ? AddOneReview(authorId)).mapTo[AddedOneReviewResponse])
+      } yield {
+        ComposedReview(review, game, user)
+      }
+
+      createReviewFuture.value.pipeTo(sender())
+
+
+    case getReviewCommand: GetReviewInfo =>
+      reviewManagerActor.forward(getReviewCommand)
+
+    case updateReviewCommand: UpdateReview =>
+      reviewManagerActor.forward(updateReviewCommand)
+
+    case deleteReviewCommand @ DeleteReview(reviewId) =>
+      val deletedReviewFuture = for {
+        review <- EitherT(getComposedReview(reviewId))
+        reviewWasDeleted <- EitherT((gameManagerActor ? deleteReviewCommand).mapTo[ReviewDeletedResponse])
+        _ <- EitherT((userManagerActor ? RemoveOneReview(review.author.userId)).mapTo[RemovedOneReviewResponse])
+      } yield {
+        reviewWasDeleted
+      }
+
+      deletedReviewFuture.value.pipeTo(sender())
+
+    case getAllReviewsByUserCommand: GetAllReviewsByAuthor =>
+      reviewManagerActor.forward(getAllReviewsByUserCommand)
+
+    case getAllReviewsByGameCommand: GetAllReviewsByGame =>
+      reviewManagerActor.forward(getAllReviewsByGameCommand)
+
+    // All CSVLoad messages
     case InitCSVLoadToManagers =>
       log.info("Initialized CSV Data load.")
       sender() ! Ack
--- src/main/scala/http/ReviewRouter.scala
+++ src/main/scala/http/ReviewRouter.scala
@@ -1,13 +1,11 @@
 package dev.galre.josue.akkaProject
 package http
 
-import actors.game.GameActor.{ GetGameInfo, GetGameInfoResponse }
+import actors.data.SteamManagerActor.ComposedReview
 import actors.review.ReviewManagerActor.{ GetAllReviewsByAuthor, GetAllReviewsByFilterResponse, GetAllReviewsByGame }
-import actors.user.UserActor._
 
 import akka.actor.ActorRef
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.server.{ Directives, Route }
 import akka.pattern.ask
 import akka.util.Timeout
@@ -15,12 +13,9 @@ import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.generic.auto._
 
 import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.{ Failure, Success }
 
 case class ReviewRouter(
-  reviewManagerActor: ActorRef,
-  userManagerActor:   ActorRef,
-  gameManagerActor:   ActorRef
+  steamManagerActor: ActorRef
 )
   (implicit timeout: Timeout, executionContext: ExecutionContext) extends Directives {
 
@@ -115,112 +110,23 @@ case class ReviewRouter(
     }
   }
 
-  private def createReviewAction(createReview: CreateReviewRequest): Future[ReviewCreatedResponse] =
-    (reviewManagerActor ? createReview.toCommand).mapTo[ReviewCreatedResponse]
-
-  private def addOneReviewAction(id: Long): Future[AddedOneReviewResponse] =
-    (userManagerActor ? AddOneReview(id)).mapTo[AddedOneReviewResponse]
-
-  private def deleteOneReviewAction(id: Long): Future[RemovedOneReviewResponse] =
-    (userManagerActor ? RemoveOneReview(id)).mapTo[RemovedOneReviewResponse]
+  private def createReviewAction(createReview: CreateReviewRequest): Future[Either[String, ComposedReview]] =
+    (steamManagerActor ? createReview.toCommand).mapTo[Either[String, ComposedReview]]
 
   private def updateNameAction(id: Long, updateReview: UpdateReviewRequest): Future[ReviewUpdatedResponse] =
-    (reviewManagerActor ? updateReview.toCommand(id)).mapTo[ReviewUpdatedResponse]
+    (steamManagerActor ? updateReview.toCommand(id)).mapTo[ReviewUpdatedResponse]
 
   private def getReviewInfoAction(id: Long): Future[GetReviewInfoResponse] =
-    (reviewManagerActor ? GetReviewInfo(id)).mapTo[GetReviewInfoResponse]
+    (steamManagerActor ? GetReviewInfo(id)).mapTo[GetReviewInfoResponse]
 
   private def deleteReviewAction(id: Long): Future[ReviewDeletedResponse] =
-    (reviewManagerActor ? DeleteReview(id)).mapTo[ReviewDeletedResponse]
+    (steamManagerActor ? DeleteReview(id)).mapTo[ReviewDeletedResponse]
 
   private def getAllReviewsByUser(id: Long, page: Int, perPage: Int): Future[GetAllReviewsByFilterResponse] =
-    (reviewManagerActor ? GetAllReviewsByAuthor(id, page, perPage)).mapTo[GetAllReviewsByFilterResponse]
+    (steamManagerActor ? GetAllReviewsByAuthor(id, page, perPage)).mapTo[GetAllReviewsByFilterResponse]
 
   private def getAllReviewsByGame(id: Long, page: Int, perPage: Int): Future[GetAllReviewsByFilterResponse] =
-    (reviewManagerActor ? GetAllReviewsByGame(id, page, perPage)).mapTo[GetAllReviewsByFilterResponse]
-
-  private def checkIfAuthorIsValid(authorId: Long)(routeIfValid: Route): Route = {
-    val userInfoResponse = (userManagerActor ? GetUserInfo(authorId)).mapTo[GetUserInfoResponse]
-
-    onSuccess(
-      userInfoResponse.map {
-        case Right(_) =>
-          Success(true)
-
-        case Left(_) =>
-          Failure(
-            new IllegalArgumentException("The authorId entered is invalid, please select a valid user.")
-          )
-      }
-    ) {
-      case Success(_) =>
-        routeIfValid
-
-      case Failure(exception) =>
-        throw exception
-    }
-  }
-
-
-  private def checkIfGameIsValid(authorId: Long)(routeIfValid: Route): Route = {
-    val gameInfoResponse = (gameManagerActor ? GetGameInfo(authorId)).mapTo[GetGameInfoResponse]
-
-    onSuccess(
-      gameInfoResponse.map {
-        case Right(_) =>
-          Success(true)
-
-        case Left(_) =>
-          Failure(
-            new IllegalArgumentException("The steamAppId is invalid, please select a valid game.")
-          )
-      }
-    ) {
-      case Success(_) =>
-        routeIfValid
-
-      case Failure(exception) =>
-        throw exception
-    }
-  }
-
-  private def checkIfAuthorAndGameAreValid(steamAppId: Long, authorId: Long)(routeIfValid: Route): Route = {
-    val gameInfoResponse = (gameManagerActor ? GetGameInfo(steamAppId)).mapTo[GetGameInfoResponse]
-    val userInfoResponse = (userManagerActor ? GetUserInfo(authorId)).mapTo[GetUserInfoResponse]
-
-    onSuccess {
-      for {
-        user <- userInfoResponse
-        game <- gameInfoResponse
-      } yield {
-        (game, user) match {
-          case (Right(_), Right(_)) =>
-            Success(true)
-
-          case (Left(_), _) =>
-            Failure(
-              new IllegalArgumentException("The authorId entered is invalid, please select a valid user.")
-            )
-
-          case (_, Left(_)) =>
-            Failure(
-              new IllegalArgumentException("The steamAppId is invalid, please select a valid game.")
-            )
-
-          case (_, _) =>
-            Failure(
-              new IllegalArgumentException("Both authorId and steamAppId are invalid, please check and try again.")
-            )
-        }
-      }
-    } {
-      case Success(_) =>
-        routeIfValid
-
-      case Failure(exception) =>
-        throw exception
-    }
-  }
+    (steamManagerActor ? GetAllReviewsByGame(id, page, perPage)).mapTo[GetAllReviewsByFilterResponse]
 
   val routes: Route =
     pathPrefix("reviews") {
@@ -230,43 +136,28 @@ case class ReviewRouter(
             concat(
               path("user" / LongNumber) { authorId =>
                 paginationParameters { (page, perPage) =>
+                  onSuccess(getAllReviewsByUser(authorId, page, perPage)) {
+                    case Right(allReviews) =>
+                      complete(StatusCodes.OK, allReviews)
 
-                  checkIfAuthorIsValid(authorId) {
-                    onComplete(getAllReviewsByUser(authorId, page, perPage)) {
-                      case Success(maybeContent) =>
-                        maybeContent match {
-                          case Right(allReviews) =>
-                            complete(StatusCodes.OK, allReviews)
-
-                          case Left(failure) =>
-                            completeWithMessage(StatusCodes.BadRequest, Some(failure))
-                        }
-
-                      case Failure(_) =>
-                        completeWithMessage(StatusCodes.BadRequest, None)
-                    }
+                    case Left(failure) =>
+                      completeWithMessage(StatusCodes.BadRequest, Some(failure))
                   }
                 }
               },
               path("game" / LongNumber) { steamAppId =>
                 paginationParameters { (page, perPage) =>
-                  checkIfGameIsValid(steamAppId) {
-                    onComplete(getAllReviewsByGame(steamAppId, page, perPage)) {
-                      case Success(maybeContent) =>
-                        maybeContent match {
-                          case Right(allReviews) =>
-                            complete(allReviews)
+                  onSuccess(getAllReviewsByGame(steamAppId, page, perPage)) {
+                    case Right(allReviews) =>
+                      complete(allReviews)
 
-                          case Left(exception) =>
-                            completeWithMessage(StatusCodes.BadRequest, Some(exception))
-                        }
-
-                      case Failure(_) =>
-                        completeWithMessage(StatusCodes.BadRequest, None)
-                    }
+                    case Left(exception) =>
+                      completeWithMessage(StatusCodes.BadRequest, Some(exception))
                   }
+
                 }
               }
+
             )
           }
         },
@@ -293,54 +184,28 @@ case class ReviewRouter(
               }
             },
             delete {
-              onSuccess(getReviewInfoAction(steamReviewId)) {
-                case Right(state) =>
-                  val authorId = state.authorId
-
-                  onSuccess(deleteReviewAction(steamReviewId)) {
-                    case Right(_) =>
-
-                      onSuccess(deleteOneReviewAction(authorId)) {
-                        case Right(_) =>
-                          completeWithMessage(StatusCodes.OK, Some("Review was deleted successfully."))
-
-                        case Left(exception) =>
-                          completeWithMessage(StatusCodes.BadRequest, Some(exception))
-                      }
-
-                    case Left(exception) =>
-                      completeWithMessage(StatusCodes.BadRequest, Some(exception))
-                  }
+              onSuccess(deleteReviewAction(steamReviewId)) {
+                case Right(_) =>
+                  completeWithMessage(StatusCodes.OK, Some("Review was deleted successfully."))
 
                 case Left(exception) =>
                   completeWithMessage(StatusCodes.BadRequest, Some(exception))
               }
+
             }
           )
         },
         pathEndOrSingleSlash {
           post {
             entity(as[CreateReviewRequest]) { review =>
-              val authorId   = review.authorId
-              val steamAppId = review.steamAppId
+              onSuccess(createReviewAction(review)) {
+                case Right(composedReview) =>
+                  complete(composedReview)
 
-              checkIfAuthorAndGameAreValid(steamAppId, authorId) {
-                onSuccess(createReviewAction(review)) {
-                  case Right(steamReviewId) =>
-                    onSuccess(addOneReviewAction(authorId)) {
-                      case Right(_) =>
-                        respondWithHeader(Location(s"/reviews/$steamReviewId")) {
-                          complete(StatusCodes.Created)
-                        }
-
-                      case Left(exception) =>
-                        completeWithMessage(StatusCodes.BadRequest, Some(exception))
-                    }
-
-                  case Left(exception) =>
-                    completeWithMessage(StatusCodes.BadRequest, Some(exception))
-                }
+                case Left(exception) =>
+                  completeWithMessage(StatusCodes.BadRequest, Some(exception))
               }
+
             }
           }
         },
